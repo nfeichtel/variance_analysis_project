2a240ebda78951841f1ea585d2f6b09fc99b54af
Nick Feichtel <nfeichtel@gmail.com>
2024-10-17 14:58:44 -0400

    create overall allocation querey

---
+++ overall_allocation_percentage.sql
@@ -0,0 +1,10 @@
+-- Overall % breakdown of how the budget is allocated by category and how money is actually spent by category
+SELECT 
+    category,
+    SUM(actual_amount) actual_amount,
+    ROUND(SUM(actual_amount) / (SELECT SUM(actual_amount) FROM q2_variance) * 100, 2) percent_of_actual_spend,
+    SUM(budget_amount) budget_amount,
+    ROUND(SUM(budget_amount) / (SELECT SUM(budget_amount) FROM q2_variance) * 100, 2) percent_of_budget_amount
+FROM q2_variance
+GROUP BY category
+ORDER BY category
