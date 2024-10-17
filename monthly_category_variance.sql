bbac10acb20d22c37600835e55649722f2240248
Nick Feichtel <nfeichtel@gmail.com>
2024-10-17 14:20:59 -0400

    monthly cat var

---
+++ monthly_category_variance.sql
@@ -0,0 +1,40 @@
+-- Variance breakdown by category by month
+
+/* April 2024 Variance
+Highest negative variance are accomodation, entertainment, and dining.
+- Accommodation was difficult to predict in April due to variable travel and non-budgeted booking fees
+*/
+SELECT 
+    month,
+    category,
+    SUM(variance_amount)
+FROM q2_variance
+WHERE month = '2024-04-01'
+GROUP BY month, category
+ORDER BY
+    month,
+    SUM(variance_amount);
+
+
+SELECT 
+    month,
+    category,
+    SUM(variance_amount)
+FROM q2_variance
+WHERE month = '2024-05-01'
+GROUP BY month, category
+ORDER BY
+    month,
+    SUM(variance_amount);
+
+
+SELECT 
+    month,
+    category,
+    SUM(variance_amount)
+FROM q2_variance
+WHERE month = '2024-06-01'
+GROUP BY month, category
+ORDER BY
+    month,
+    SUM(variance_amount);
