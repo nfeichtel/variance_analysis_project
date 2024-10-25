85779acf5a672deaea2037ae293ea14f81478183
Nick Feichtel <nfeichtel@gmail.com>
2024-10-25 15:20:45 -0400

    added monthly variance percentage

--- analysis_queries/6_monthly_category_variance.sql
+++ analysis_queries/6_monthly_category_variance.sql
@@ -7,7 +7,9 @@ Highest negative variance are accomodation, entertainment, and dining.
 SELECT 
     month,
     category,
-    SUM(variance_amount)
+    SUM(variance_amount),
+    ROUND(SUM(variance_amount) /
+        (SELECT SUM(budget_amount) FROM q2_variance WHERE month = '2024-04-01')  * 100, 2) AS variance_percentage
 FROM q2_variance
 WHERE month = '2024-04-01'
 GROUP BY month, category
@@ -19,7 +21,9 @@ ORDER BY
 SELECT 
     month,
     category,
-    SUM(variance_amount)
+    SUM(variance_amount),
+    ROUND(SUM(variance_amount) / 
+        (SELECT SUM(budget_amount) FROM q2_variance WHERE month = '2024-05-01')  * 100, 2) AS variance_percentage
 FROM q2_variance
 WHERE month = '2024-05-01'
 GROUP BY month, category
@@ -31,10 +35,10 @@ ORDER BY
 SELECT 
     month,
     category,
-    SUM(variance_amount)
+    SUM(variance_amount),
+    ROUND(SUM(variance_amount) / 
+        (SELECT SUM(budget_amount) FROM q2_variance WHERE month = '2024-06-01')  * 100, 2) AS variance_percentage
 FROM q2_variance
 WHERE month = '2024-06-01'
 GROUP BY month, category
-ORDER BY
-    month,
-    SUM(variance_amount);
+ORDER BY SUM(variance_amount);
