ae8d3f96097a84db960257081ac4e6b3a2fa952b
Nick Feichtel <nfeichtel@gmail.com>
2024-10-29 14:04:49 -0400

    added percent variance

--- analysis_queries/1_overall_category_variance.sql
+++ analysis_queries/1_overall_category_variance.sql
@@ -1,7 +1,8 @@
 --Highest and lowest total Q2 2024 variances by category
 SELECT 
     category,
-    SUM(variance_amount) AS total_variance
+    SUM(variance_amount) AS total_variance,
+    ROUND(SUM(variance_amount)/SUM(budget_amount) * 100, 2) AS percent_variance
 FROM q2_variance
 GROUP BY category
-ORDER BY SUM(variance_amount)
+ORDER BY ROUND(SUM(variance_amount)/SUM(budget_amount) * 100, 2)
