9c1ab9eaeb0e4f7edfdabe9bd5ade17f50961fb9
Nick Feichtel <nfeichtel@gmail.com>
2024-10-29 14:40:00 -0400

    added total_variance alias

--- analysis_queries/3_overall_ranking_categories.sql
+++ analysis_queries/3_overall_ranking_categories.sql
@@ -1,7 +1,7 @@
 -- Ranking the categories by total variance for Q2 2004
 SELECT
     category,
-    SUM(variance_amount),
+    SUM(variance_amount) AS total_variance,
     RANK() OVER(PARTITION BY favorability ORDER BY ABS(SUM(variance_amount))DESC) AS variance_rank
 FROM q2_variance
 GROUP BY category, favorability;
